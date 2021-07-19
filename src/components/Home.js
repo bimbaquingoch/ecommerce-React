03208bfb9beb11fbbe8dc26a39511b16f730ac3e
Bryan Imbaquingo <bryan_3871@hotmail.com>
2021-07-19 14:18:27 -0500

    carousel

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { FiltroCategoria } from "./FiltoCategoria";
 import ProductItem from "./ProductItem";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 import "../styles/index.css";
 import "animate.css";
 
@@ -8,15 +10,39 @@ export const Home = () => {
   const [filtro, setFiltro] = useState(["men's clothing"]);
 
   return (
-    <main>
-      <section className="btn-categorias">
-        <FiltroCategoria setFiltro={setFiltro} />
-      </section>
-      <section className="cards-products">
-        {filtro.map((elmnt) => (
-          <ProductItem key={elmnt} filtro={filtro} />
-        ))}
-      </section>
-    </main>
+    <>
+      <Carousel showThumbs={false}>
+        <div className="carousel">
+          <img
+            src="https://images.pexels.com/photos/994517/pexels-photo-994517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750"
+            alt="laptop"
+          />
+        </div>
+
+        <div className="carousel">
+          <img
+            src="https://images.pexels.com/photos/6069083/pexels-photo-6069083.jpeg"
+            alt="laptop"
+          />
+        </div>
+
+        <div className="carousel">
+          <img
+            src="https://images.pexels.com/photos/325153/pexels-photo-325153.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+            alt="laptop"
+          />
+        </div>
+      </Carousel>
+      <main>
+        <section className="btn-categorias">
+          <FiltroCategoria setFiltro={setFiltro} />
+        </section>
+        <section className="cards-products">
+          {filtro.map((elmnt) => (
+            <ProductItem key={elmnt} filtro={filtro} />
+          ))}
+        </section>
+      </main>
+    </>
   );
 };
